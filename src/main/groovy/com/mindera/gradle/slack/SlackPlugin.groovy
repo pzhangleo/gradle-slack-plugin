3c4306ce443420f81d337a1567e060979e0d0823
pzhang <pengzh.leo@gmail.com>
2016-05-20 17:53:52 +0800

    修改参数

--- src/main/groovy/com/mindera/gradle/slack/SlackPlugin.groovy
+++ src/main/groovy/com/mindera/gradle/slack/SlackPlugin.groovy
@@ -57,8 +57,7 @@ class SlackPlugin implements Plugin<Project> {
         // only send a slack message if the task failed
         // or the task is registered to be monitored
         if (shouldSendMessage) {
-            SlackMessage slackMessage = SlackMessageTransformer.buildSlackMessage(mExtension.title, task, state, 
-                mTaskLogBuilder.toString(), mExtension.message)
+            SlackMessage slackMessage = SlackMessageTransformer.buildSlackMessage(mExtension.title, task, state, mTaskLogBuilder.toString(), mExtension.message)
             SlackApi api = new SlackApi(mExtension.url)
             api.call(slackMessage)
         }
