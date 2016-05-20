325149efa6702370ce15fa6a4f1ed22e9d2c6d2c
pzhang <pengzh.leo@gmail.com>
2016-05-20 17:44:19 +0800

    add method param

--- src/main/groovy/com/mindera/gradle/slack/SlackPlugin.groovy
+++ src/main/groovy/com/mindera/gradle/slack/SlackPlugin.groovy
@@ -57,7 +57,8 @@ class SlackPlugin implements Plugin<Project> {
         // only send a slack message if the task failed
         // or the task is registered to be monitored
         if (shouldSendMessage) {
-            SlackMessage slackMessage = SlackMessageTransformer.buildSlackMessage(mExtension.title, task, state, mTaskLogBuilder.toString())
+            SlackMessage slackMessage = SlackMessageTransformer.buildSlackMessage(mExtension.title, task, state, 
+                mTaskLogBuilder.toString(), mExtension.message)
             SlackApi api = new SlackApi(mExtension.url)
             api.call(slackMessage)
         }
